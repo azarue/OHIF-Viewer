005e30c50f10182eca3b928143481182963f0469
Adiel Cohen <adiel@siteflex.co.il>
2025-06-21 11:24:08 -0400

    Updated Google production configuration file to not allow oAuth

--- platform/app/public/config/secopsys-google-prod.js
+++ platform/app/public/config/secopsys-google-prod.js
@@ -13,23 +13,23 @@ window.config = {
   showLoadingIndicator: true,
   strictZSpacingForVolumeViewport: true,
   // This is an array, but we'll only use the first entry for now
-  oidc: [
-    {
-      // ~ REQUIRED
-      // Authorization Server URL
-      authority: 'https://accounts.google.com',
-      client_id: '6194423968-h27rp9e6qnck95j3sef3dpbk6n7culp0.apps.googleusercontent.com',
-      redirect_uri: '/callback',
-      response_type: 'id_token token',
-      scope:
-        'email profile openid https://www.googleapis.com/auth/cloudplatformprojects.readonly https://www.googleapis.com/auth/cloud-healthcare', // email profile openid
-      // ~ OPTIONAL
-      post_logout_redirect_uri: '/logout-redirect.html',
-      revoke_uri: 'https://accounts.google.com/o/oauth2/revoke?token=',
-      automaticSilentRenew: true,
-      revokeAccessTokenOnSignout: true,
-    },
-  ],
+  // oidc: [
+  //   {
+  //     // ~ REQUIRED
+  //     // Authorization Server URL
+  //     authority: 'https://accounts.google.com',
+  //     client_id: '6194423968-h27rp9e6qnck95j3sef3dpbk6n7culp0.apps.googleusercontent.com',
+  //     redirect_uri: '/callback',
+  //     response_type: 'id_token token',
+  //     scope:
+  //       'email profile openid https://www.googleapis.com/auth/cloudplatformprojects.readonly https://www.googleapis.com/auth/cloud-healthcare', // email profile openid
+  //     // ~ OPTIONAL
+  //     post_logout_redirect_uri: '/logout-redirect.html',
+  //     revoke_uri: 'https://accounts.google.com/o/oauth2/revoke?token=',
+  //     automaticSilentRenew: true,
+  //     revokeAccessTokenOnSignout: true,
+  //   },
+  // ],
   extensions: [],
   modes: [],
   showStudyList: false,
